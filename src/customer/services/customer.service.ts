82793e3a4ba20d30839f448fd11cf29fabb32d59
Cleber Del Rei <cleberdelrei@gmail.com>
2021-06-12 23:34:03 -0300

    feat(customer): endpoints & entities

--- src/customer/services/customer.service.ts
+++ src/customer/services/customer.service.ts
@@ -1,35 +1,61 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
 import { Transactional } from "typeorm-transactional-cls-hooked";
-import { CustomerDto } from "../dto/customer.dto";
+import { CreateCustomerDto } from "../dto/create-customer.dto";
+import { UpdateCustomerDto } from "../dto/update-customer.dto";
 import { Customer } from "../entities/customer.entity";
+import { Student } from "../entities/student.entity";
 import { CustomerRepository } from "../repository/customer.repository";
+import { StudentRepository } from "../repository/student.repository";
 
 @Injectable()
 export class CustomerService {
   constructor(
     @InjectRepository(Customer)
-    private readonly customerRepository: CustomerRepository
+    private readonly customerRepository: CustomerRepository,
+    @InjectRepository(Student)
+    private readonly studentRepository: StudentRepository
   ) {}
+  async create(customerDto: CreateCustomerDto): Promise<Customer> {
+    return customerDto.organization
+      ? await this.studentRepository.save(customerDto)
+      : await this.customerRepository.save(customerDto);
+  }
 
-  async create(customerDto: CustomerDto): Promise<Customer> {
-    const customer = this.customerRepository.create(customerDto);
-    return this.customerRepository.save(customer);
-  }
   @Transactional()
-  async update(id: number, customerDto: CustomerDto): Promise<Customer> {
-    const customer = this.customerRepository.create(customerDto);
-    await this.customerRepository.update(id, customer);
-    return customer;
+  async update(id: number, customerDto: UpdateCustomerDto): Promise<Customer> {
+    let customer = await this.findOne(id);
+    if (customer instanceof Student) {
+      customer = this.studentRepository.merge(customer, customerDto);
+      return this.studentRepository.save(customer);
+    } else {
+      customer = this.customerRepository.merge(customer, customerDto);
+      return this.customerRepository.save(customer);
+    }
   }
+
   async findAll(): Promise<Customer[]> {
     return this.customerRepository.find();
   }
-  async findOne(id: number): Promise<Customer | undefined> {
-    return this.customerRepository.findOne(id);
+
+  async findOne(id: number): Promise<Customer> {
+    const customer = await this.studentRepository.findOne({
+      select: ["organization"],
+      where: { id },
+    });
+   
+    const result = (customer
+    ? await this.studentRepository.findOne(id)
+    : await this.customerRepository.findOne(id))!;
+
+    if (!result) throw new NotFoundException();
+
+    return result
   }
+
   @Transactional()
   async remove(id: number): Promise<void> {
-    await this.customerRepository.delete(id);
+    const result = await this.customerRepository.delete(id);
+    if (!result.affected) throw new NotFoundException();
   }
 }
